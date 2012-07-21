7029529452ae63dc03518dc9a0a9ee7a49c00153
Brian Hatfield <bhatfield@brightcove.com>
2012-07-21 10:17:46 -0400

    Update Manager

--- daemon/manager.py
+++ daemon/manager.py
@@ -1,6 +1,6 @@
 import clientqueue.queue
 
-q = clientqueue.queue.SQSQueue('chef-client-deregistration-queue-arctic', 'AKIAIU5C2XWX3COAUK3A', '29zbMV3uzNMsa5eoH+8JEI08EuFoXpUkX16+QIhO')
+q = clientqueue.queue.SQSQueue('chef-client-deregistration-queue-arctic', 'ACCESS_KEY', 'SECRET_KEY')
 
 q.enqueue("test message")
 
