8cac1417c25a4f02474743792df03bf71d6924e5
Brian Hatfield <bhatfield@brightcove.com>
2012-09-04 20:39:59 -0400

    Refactor message handler to deal with multiple 'message' types.

--- lib/message/handler.py
+++ lib/message/handler.py
@@ -12,10 +12,50 @@ class ChefRegistrationHandler():
         self.backup_dir = backup_dir
 
     def process(self, message):
-        if message.message["type"] == "registration" and message.message["method"] == "deregister":
-            self.deregister(message)
+        if message.format.__class__.__name__ == "AutoscalingMessage":
+            nodes = chef.Search("node", q="instance_id:%s" % (message.message['EC2InstanceId']))
+            if nodes.total == 1:
+                self._remove(chef_name=nodes[0]['name'], instance_id=message.message['EC2InstanceId'])
+            else:
+                raise ValueError("API Returned %s nodes, expected 1" % (nodes.total))
 
-    def backup(self, filename, contents):
+        elif message.format.__class__.__name__ == "RegistrationMessage":
+            if message.message["method"] == "deregister":
+                if 'instance_id' in message.message:
+                    instance_id = message.message['instance_id']
+                else:
+                    instance_id = None
+
+                self._remove(chef_name=message.message['chef_name'], instance_id=instance_id)
+
+        else:
+            raise NotImplementedError("Unable to process message type: %s" % (message.format.__class__.__name__))
+
+    def _remove(self, chef_name, instance_id=None, backup=True):
+        try:
+            client = chef.Client(chef_name)
+            if backup and self._backup("chef-client-%s" % (chef_name), json.dumps(client.to_dict())):
+                client.delete()
+        except chef.exceptions.ChefServerNotFoundError:
+            logging.error("Client removal requested for non-existent chef client '%s'", chef_name)
+        except Exception as e:
+            logging.exception("Exception while deleting chef client:\n %s", str(e))
+
+        try:
+            node = chef.Node(chef_name)
+            if instance_id:
+                if node.attributes["ec2"]["instance_id"] != instance_id:
+                    raise ValueError("Instance ID (%s) does not match attribute ID (%s)" %
+                                    (instance_id, node.attributes["ec2"]["instance_id"]))
+
+            if backup and self._backup("chef-node-%s" % (chef_name), json.dumps(node.attributes.to_dict())):
+                node.delete()
+        except chef.exceptions.ChefServerNotFoundError:
+            logging.error("Node removal requested for non-existent chef node '%s'", chef_name)
+        except Exception as e:
+            logging.exception("Exception while deleting chef node:\n %s", str(e))
+
+    def _backup(self, filename, contents):
         if os.path.exists(self.backup_dir) and os.path.isdir(self.backup_dir):
             logging.info("Writing backup: %s", os.path.join(self.backup_dir, filename))
             handle = open(os.path.join(self.backup_dir, "%s-%s" % (filename, int(time.time()))), 'w')
@@ -23,28 +63,3 @@ class ChefRegistrationHandler():
             return True
         else:
             raise RuntimeError("Unable to access backup path: '%s'" % (self.backup_dir))
-
-    def deregister(self, message):
-        try:
-            node = chef.Node(message.message["chef_name"])
-
-            if "instance_id" in message.message:
-                if node.attributes["ec2"]["instance_id"] != message.message["instance_id"]:
-                    raise ValueError("Instance ID provided (%s) does not match Chef's EC2 ID (%s) for this node")
-
-            if self.backup("chef-node-%s" % (message.message["chef_name"]), json.dumps(node.attributes.to_dict())):
-                node.delete()
-        except chef.exceptions.ChefServerNotFoundError:
-            logging.error("Node removal requested for non-existent chef node '%s'", message.message["chef_name"])
-        except Exception as e:
-            logging.exception("Exception while deleting chef node:\n %s", str(e))
-
-        try:
-            client = chef.Client(message.message["chef_name"])
-            if self.backup("chef-client-%s" % (message.message["chef_name"]), json.dumps(client.to_dict())):
-                client.delete()
-        except chef.exceptions.ChefServerNotFoundError:
-            logging.error("Client removal requested for non-existent chef client '%s'", message.message["chef_name"])
-        except Exception as e:
-            logging.exception("Exception while deleting chef client:\n %s", str(e))
-
