0b19d00d3ff8123bc3320a1fe461d0826ff0f241
Brian Hatfield <bhatfield@brightcove.com>
2012-09-21 13:46:53 -0400

    Add support for creating SQS queues that are subscribed to SNS topics.
    This change is a bit lengthy because it works around poor modeling of Amazon's SQS/SNS results by Boto. It also works around a major bug in Boto that fails to properly modify existing SQS policies, by flushing all existing policies on an SQS queue before attempting to resubscribe.
    
    Finally, it makes sure that the topic, queue, and their cross subscriptions exist.

--- lib/clientqueue/queue.py
+++ lib/clientqueue/queue.py
@@ -1,3 +1,4 @@
+import json
 import logging
 import boto.sqs.connection
 import boto.sqs.message
@@ -35,6 +36,7 @@ class SQSQueue(Queue):
             logging.info("Creating queue: %s", queue_name)
             self.queue = self.sqs.create_queue(queue_name)
 
+        self.queue_arn = self.queue.get_attributes()['QueueArn']
         self.queue.set_message_class(boto.sqs.message.RawMessage)
 
     def enqueue(self, message):
@@ -72,3 +74,33 @@ class AutoscalingQueue(SQSQueue):
     def __init__(self, queue_name, access_key, secret_key):
         SQSQueue.__init__(self, queue_name, access_key, secret_key)
         self.sns = boto.sns.SNSConnection(access_key, secret_key)
+
+        self.topic = self.sns.create_topic(queue_name)
+        self.topic_arn = self.topic['CreateTopicResponse']['CreateTopicResult']['TopicArn']
+
+        s = self.sns.get_all_subscriptions_by_topic(self.topic_arn)
+        self.subscriptions = [sub['Endpoint'] for sub in s['ListSubscriptionsByTopicResponse']['ListSubscriptionsByTopicResult']['Subscriptions']]
+
+        p = json.loads(self.queue.get_attributes('Policy')['Policy'])
+        self.permitted_topics = [s['Condition'][d]['aws:SourceArn'] for d in [c for s in p['Statement'] for c in s['Condition']]]
+
+        if self.topic_arn not in self.permitted_topics:
+            logging.warning("Topic ARN %s not permitted to write to queue (%s)", self.topic_arn, self.name)
+            logging.warning("Flushing all existing policies on %s", self.name)
+            self.queue.set_attribute('Policy', '')
+
+            logging.info("Subscribing queue (%s) to topic (%s)", self.name, self.topic_arn)
+            self.sns.subscribe_sqs_queue(self.topic_arn, self.queue)
+        else:
+            logging.debug("Topic ARN (%s) permitted against SQS queue (%s)", self.topic_arn, self.name)
+            if self.queue_arn not in self.subscriptions:
+                logging.warning("%s is not subscribed to %s", self.name, self.topic_arn)
+                logging.info("Subscribing %s to %s", self.name, self.topic_arn)
+                self.sns.subscribe(self.topic_arn, 'SQS', self.queue_arn)
+
+
+
+
+
+
+
